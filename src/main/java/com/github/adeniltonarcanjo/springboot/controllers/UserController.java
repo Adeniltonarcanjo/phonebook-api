018b818eef458cd7288f397e06d1247e94e52140
Adeniltonarcanjo <Adeniltonmorais@gmail.com>
2022-05-26 23:31:34 -0300

    ropository

--- src/main/java/com/github/adeniltonarcanjo/springboot/controllers/UserController.java
+++ src/main/java/com/github/adeniltonarcanjo/springboot/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.github.adeniltonarcanjo.springboot.controllers;
 
 import com.github.adeniltonarcanjo.springboot.model.User;
+import com.github.adeniltonarcanjo.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
@@ -15,28 +17,34 @@ public class UserController {
 
     private List<User> listUser= new ArrayList<>();
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/{id}")
     public User user(@PathVariable("id") Long id ) {
-        Optional <User> userFind=listUser.stream().filter(user->user.getId()==id).findFirst();
+        Optional <User> userFind= this.userRepository.findById(id);
 
         if (userFind.isPresent()){
             return userFind.get();
         }else {
             return null;
         }
-
-
     }
 
     @PostMapping("/")
     public User user(@RequestBody User user){
-        listUser.add(user);
-        return user;
+        return this.userRepository.save(user);
+
     }
 
     @GetMapping("/list")
     public List<User> userList(){
-        return listUser;
+        return this.userRepository.findAll();
+    }
+
+    @GetMapping("/list/{id}")
+    public List<User> userListMoreThan(@PathVariable("id") Long id){
+        return this.userRepository.findAllMoreThan(id);
     }
 
 
--- src/main/java/com/github/adeniltonarcanjo/springboot/model/User.java
+++ src/main/java/com/github/adeniltonarcanjo/springboot/model/User.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String username;
